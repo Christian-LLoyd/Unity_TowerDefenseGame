48e100e73adf8dfa8025c36a1cb1f74ceafaf6d9
YToh <galabayanthony92@gmail.com>
2025-02-11 22:06:27 +0800

    added some assets
    added some assets

--- Tower_Defense_game/Assets/Scripts/Enemy_Controller.cs
+++ Tower_Defense_game/Assets/Scripts/Enemy_Controller.cs
@@ -2,37 +2,25 @@ using UnityEngine;
 
 public class Enemy_Controller : MonoBehaviour
 {
-
     public float moveSpeed;
     public Transform target;
-    private Path thePath;
-     private int currentPoint;
-    private bool reachedEnd;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       thePath = FindAnyObjectByType<Path>();
-
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!reachedEnd)
-        {
-            //transform.LookAt(thePath.points[currentPoint]);
-            transform.position = Vector3.MoveTowards(transform.position, thePath.points[currentPoint].position, moveSpeed * Time.deltaTime);
+        // Move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
 
-        if(Vector3.Distance(transform.position, thePath.points[currentPoint].position) < 0.1f)
+        
+        Vector3 direction = (target.position - transform.position).normalized;
+        if (direction != Vector3.zero) 
         {
-            currentPoint = currentPoint + 1;
-            if (currentPoint >= thePath.points.Length)
-            {
-                reachedEnd = true;
-            }
-        }
-
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
         }
     }
 }
